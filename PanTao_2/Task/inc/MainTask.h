852f448217550d8e98322365fb881e432f6250cb
Lajgit <3243570032@qq.com>
2026-07-11 17:18:26 +0800

    同步场景枚举定义

--- PanTao_2/Task/inc/MainTask.h
+++ PanTao_2/Task/inc/MainTask.h
@@ -4,10 +4,9 @@
 
 typedef enum
 {
-    SCENE_LESSLIGHT = 0,
+    SCENE_SETTING = 0,
     SCENE_IDLE = 1,
     SCENE_PLAYING = 2,
-    SCENE_VICTORY = 3,
 }Scene_t;
 
 #define Event_SaveSetting (1u << 0) 
